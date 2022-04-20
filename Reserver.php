280ddf48b452fb59926af5961b9ab3654fbe8b01
Valentin <75315884+Contunder@users.noreply.github.com>
2022-04-20 23:13:50 +0200

    MaJ

--- Reserver.php
+++ Reserver.php
@@ -14,7 +14,7 @@ $chambreDispo = getChambreDispo($conn);
   	<title>EASTMASTER - RÉSERVATION</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+      <link rel="icon" href="Img/East/logo.jpg" type="image/png">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
